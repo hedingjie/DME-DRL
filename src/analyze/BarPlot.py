0df9dbe27c12b0c78cba4deecf36ca6b01d21b69
hdj <dingjiehe@gmail.com>
2020-07-21 08:04:48 +0800

    barplot

--- src/analyze/BarPlot.py
+++ src/analyze/BarPlot.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import seaborn as sn
 import numpy as np
 
-data = pd.read_csv('/home/hdj/Desktop/实验记录/DistanceCost/comparision (copy).csv')
-data.sort_values(by='Distance Traveled')
-sn.barplot(x=data['Map ID'], y=data['Distance Traveled'], hue=data['Method'], data=data, capsize=.2)
+data = pd.read_csv('/home/hdj/Git/DME-DRL/src/results/comparision (copy).csv')
+sn.barplot(x='Map ID', y='Distance Traveled', hue='Method', data=data)
 plt.show()
